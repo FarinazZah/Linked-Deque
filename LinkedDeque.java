170846e04d5c3886a6d58b7548e1d98e62c69a6b
Farinaz Zahiri <163356393+FarinazZah@users.noreply.github.com>
2025-04-11 21:02:47 -0400

    Update LinkedDeque.java

--- LinkedDeque.java
+++ LinkedDeque.java
@@ -1,17 +1,4 @@
 import java.util.NoSuchElementException;
-
-/**
- * Your implementation of a LinkedDeque.
- *
- * @author Farinaz Zahiri
- * @version 1.0
- * @userid fzahiri6
- * @GTID 903687032
- *
- * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
- *
- * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
- */
 public class LinkedDeque<T> {
 
     // Do not add new instance variables or modify existing ones.
